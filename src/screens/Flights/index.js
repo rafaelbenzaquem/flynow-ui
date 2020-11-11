62523558640e2fbb6afb90c4b576c1cb61598193
Rafael Benzquem Neto <rafael.neto@outlook.com>
2020-11-11 14:05:19 -0300

    added Fleight screen and yours components

--- src/screens/Flights/index.js
+++ src/screens/Flights/index.js
@@ -1,21 +1,58 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { Container } from './styles';
+import {
+    Container,
+    Scroller,
+
+    HeaderAerea,
+    HeaderTitle,
+
+    SearchArea,
+    OriginFinder,
+    OriginInput,
+    DestinationFinder,
+    DestinationInput,
+    ReverseRoute
+} from './styles';
 import { Api } from '../../Api';
+
+import Icon1 from './asserts/icon_1.svg';
+import Icon2 from './asserts/icon_2.svg';
+import Icon3 from './asserts/icon_3.svg';
+import Icon4 from './asserts/icon_4.svg';
+import Icon5 from './asserts/icon_5.svg';
 
 export default () => {
     const findTravels = async () => {
         let json = await Api.findServicesByType('travel');
-
         return json;
     }
-
-    findTravels();
-
     return (
         <Container>
-            <Text>Flights</Text>
-            
+            <Scroller>
+                <HeaderAerea>
+                    <Icon1 />
+                    <HeaderTitle >Flights</HeaderTitle>
+                </HeaderAerea>
+                <SearchArea>
+                    
+                    <OriginFinder>
+                        <Icon2 />
+                    </OriginFinder>
+                    <OriginInput></OriginInput>
+                    
+                   
+                    <DestinationFinder>
+                        <Icon3 />
+                    </DestinationFinder>
+                    <DestinationInput></DestinationInput>
+                    
+                    <ReverseRoute>
+                        <Icon4 />
+                    </ReverseRoute>
+
+                </SearchArea>
+            </Scroller>
         </Container>
     );
 }
--- src/screens/Flights/styles.js
+++ src/screens/Flights/styles.js
@@ -1,4 +1,42 @@
 import React from 'react';
 import styled from 'styled-components/native';
 
-export const Container = styled.SafeAreaView``;
+export const Container = styled.SafeAreaView`
+    flex:1;
+    background-color:#F5FDFE;
+`;
+
+export const Scroller = styled.ScrollView`
+    flex:1;
+    padding:20px;
+
+`;
+export const HeaderAerea = styled.View`
+    flex-direction:row;
+    align-items:center;
+`;
+export const HeaderTitle = styled.Text`
+    font-size:20;
+    font-family:'Lato';
+    font-weight:bold;
+    color:#00C6EB;
+`;
+export const SearchArea = styled.View`
+    background-color:#FFF;
+    height:210px;
+    border-radius:10px;
+    border:1px;
+    flex-direction:row;
+
+    border-color:#00C6EB;
+
+
+`;
+export const OriginFinder = styled.TouchableOpacity`
+flex:1;
+
+`;
+export const OriginInput = styled.TextInput``;
+export const DestinationFinder = styled.TouchableOpacity``;
+export const DestinationInput = styled.TextInput``;
+export const ReverseRoute = styled.TouchableOpacity``;
